5e3829f5b80040578fefb659265dd5e6b2db0b65
Diego <Xerlocs@yahoo.com>
2023-09-14 09:30:33 -0300

    Tercer Commit (pormientras)

---
+++ app/src/main/java/cl/dv/bopit/PreferenceActivity.kt
@@ -0,0 +1,11 @@
+package cl.dv.bopit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preference)
+    }
+}
---
+++ app/src/main/java/cl/dv/bopit/SplashActivity.kt
@@ -0,0 +1,11 @@
+package cl.dv.bopit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
