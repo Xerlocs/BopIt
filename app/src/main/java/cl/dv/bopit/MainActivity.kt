75bcaf6e63275b5544dc948df8ca96ac1e180c74
Diego <Xerlocs@yahoo.com>
2023-11-12 19:30:45 -0300

    Commit 11 (limpieza y actualizacion)

--- app/src/main/java/cl/dv/bopit/MainActivity.kt
+++ app/src/main/java/cl/dv/bopit/MainActivity.kt
@@ -4,36 +4,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
-        //About
-        val aboutNavigate = findViewById<Button>(R.id.aboutButton)
-
-        aboutNavigate.setOnClickListener{
-            val intentAbout = Intent(this, AboutActivity::class.java)
-            startActivity(intentAbout)
-        }
-
-        //Preferencias
-        val prefNavigate = findViewById<Button>(R.id.prefButton)
-
-        prefNavigate.setOnClickListener{
-            val intentAbout = Intent(this, SettingsActivity::class.java)
-            startActivity(intentAbout)
-        }
-
-        //sonidos
+        //juego
         val gameNavigate = findViewById<Button>(R.id.juegoButton)
 
         gameNavigate.setOnClickListener{
-            val intentAbout = Intent(this, Juego::class.java)
-            startActivity(intentAbout)
+            val intentGame = Intent(this, Juego::class.java)
+            startActivity(intentGame)
         }
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                val intentSettings = Intent(this, SettingsActivity::class.java)
+                startActivity(intentSettings)
+                return true
+            }
+            R.id.action_about -> {
+                val intentAbout = Intent(this, AboutActivity::class.java)
+                startActivity(intentAbout)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/cl/dv/bopit/SettingsActivity.kt
+++ app/src/main/java/cl/dv/bopit/SettingsActivity.kt
@@ -9,6 +9,7 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
